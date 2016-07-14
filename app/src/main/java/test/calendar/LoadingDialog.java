38217d9c906d56f39e1952270dfefebfb68569c0
Meshugah <vkarthi@purdue.edu>
2016-07-14 17:42:22 +0530

    Lol make sure you debug the login activity

---
+++ app/src/main/java/test/calendar/LoadingDialog.java
@@ -0,0 +1,33 @@
+package test.calendar;
+
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoadingDialog extends AppCompatActivity {
+
+    private ProgressDialog mProgressDialog;
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        hideProgressDialog();
+    }
+
+}
--- app/src/main/java/test/calendar/SignupActivity.java
+++ app/src/main/java/test/calendar/SignupActivity.java
@@ -3,12 +3,10 @@ package test.calendar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,7 +15,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class SignupActivity extends BaseActivity implements View.OnClickListener {
+public class SignupActivity extends LoadingDialog implements View.OnClickListener {
 
     private static final String TAG = "EmailPassword";
 
