05d1b16a02ac8875e785ad7b60adf913ad23fd8d
Meshugah <vkarthi@purdue.edu>
2016-07-20 17:57:23 +0530

    User Screen now exists.  -Must do group activities. -Lol make sure you debug the login activity

---
+++ app/src/main/java/test/calendar/UserPage.java
@@ -0,0 +1,135 @@
+package test.calendar;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class UserPage extends ActionBarActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+
+    private FirebaseAuth mAuth;
+    private ListView mDrawerList;
+    ViewPager pager;
+    private String[] titles = new String[]{"Sample Tab 1", "Sample Tab 2", "Sample Tab 3", "Sample Tab 4"
+            , "Sample Tab 5", "Sample Tab 6", "Sample Tab 7", "Sample Tab 8"};
+    private Toolbar toolbar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+
+        mAuth = FirebaseAuth.getInstance();
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.navdrawer);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            toolbar.setNavigationIcon(R.drawable.ic_dots);
+        }
+//        pager = (ViewPager) findViewById(R.id.viewpager);
+//        slidingTabLayout = (SlidingTabLayout) findViewById(R.id.sliding_tabs);
+//        pager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager(), titles));
+//
+//        slidingTabLayout.setViewPager(pager);
+//        slidingTabLayout.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+//            @Override
+//            public int getIndicatorColor(int position) {
+//                return Color.WHITE;
+//            }
+//        });
+        drawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        mDrawerLayout.setDrawerListener(drawerToggle);
+        String[] values = new String[]{
+                "ABOUT", "FACILITIES", "CALENDAR", "LOGOUT"
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        mDrawerList.setAdapter(adapter);
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                switch (position) {
+                    case 0:
+                        //ABOUT
+                        Intent in4 = new Intent(getApplicationContext(),About.class);
+                        startActivity(in4);
+                        break;
+                    case 1:
+                        //FACILITIES
+                        Intent in3 = new Intent(getApplicationContext(),Facilities.class);
+                        startActivity(in3);
+
+                        break;
+                    case 2:
+                        //CALENDAR
+                        Intent in2 = new Intent(getApplicationContext(),Calendar.class);
+                        startActivity(in2);
+                        break;
+                    case 3:
+                        //LOGOUT
+                        mAuth.signOut();
+                        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                        //Intent in = new Intent(getApplicationContext(),Facilities.class);
+                        startActivity(intent);
+
+                      /*  mDrawerList.setBackgroundColor(getResources().getColor(R.color.material_blue_grey_800));
+                        toolbar.setBackgroundColor(getResources().getColor(R.color.material_blue_grey_800));
+                        slidingTabLayout.setBackgroundColor(getResources().getColor(R.color.material_blue_grey_800));
+                        mDrawerLayout.closeDrawer(Gravity.START);*/
+
+                        break;
+                }
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+}
